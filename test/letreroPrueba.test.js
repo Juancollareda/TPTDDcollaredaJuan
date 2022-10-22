a7bef6a6a18ff89ccb6f2c54f2773303233d000b
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 07:46:09 -0300

    TDD iteration

--- test/letreroPrueba.test.js
+++ test/letreroPrueba.test.js
@@ -4,3 +4,8 @@ test("crear tablero vacio", ()=>{
     expect(tablero.crear()).toBe(null); 
 
 })
+test("crear tablero 10*10", ()=>{
+    const tablero = new Tablero();
+    expect(tablero.crear(4,4)).toBe([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]); 
+
+})
