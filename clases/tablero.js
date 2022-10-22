ddb53b98267e508e1cacc59ad11135283080a7a4
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 08:04:35 -0300

    TDD iteration

--- clases/tablero.js
+++ clases/tablero.js
@@ -4,7 +4,7 @@ function Tablero(){
     this.crear=function(fila,columna){
     for (var i = 0; i < fila; i++) {
         for (var j = 0; j < columna; j++) {
-            this.luces="0"+this.luces
+            this.luces=this.luces+'0'
 
    }
 
