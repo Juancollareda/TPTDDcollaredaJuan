a20ec3ce7297cd8e66946efd9b721b1ce09e6e47
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 09:58:37 -0300

    TDD iteration

---
+++ clases/tablero2.js
@@ -0,0 +1,70 @@
+function Tablero(){
+
+    this.luces=[]
+    this.crear=function(fila,columna){
+        if(fila==null && columna==null){
+            return null
+        }
+    for (var i = 0; i < fila; i++) {
+        this.luces.push([])
+        for (var j = 0; j < columna; j++) {
+            this.luces[i].push(0)
+
+   }
+   
+}
+return this.luces
+    }
+this.prender=function(posinicial,posfinal){
+
+
+    for(var i=posinicial[0];i<=posfinal[0];i++){
+        for (var j = posinicial[1]; j<=posfinal[1]; j++){
+
+            this.luces[i][j]=1
+
+        }
+    }
+return this.luces
+}
+this.apagar=function(posinicial,posfinal){
+
+
+    for(var i=posinicial[0];i<=posfinal[0];i++){
+        for (var j = posinicial[1]; j<=posfinal[1]; j++){
+
+            this.luces[i][j]=0
+
+        }
+    }
+return this.luces
+}
+this.cambiar=function(posinicial,posfinal){
+
+
+    for(var i=posinicial[0];i<=posfinal[0];i++){
+        for (var j = posinicial[1]; j<=posfinal[1]; j++){
+
+            this.luces[i][j]=(this.luces[i][j]==0)?1:0;
+        }
+    
+    }
+return this.luces
+
+}
+this.cuantasEncendidas=function(posinicial,posfinal){
+    cont=0
+
+    for(var i=posinicial[0];i<=posfinal[0];i++){
+        for (var j = posinicial[1]; j<=posfinal[1]; j++){
+
+            cont=this.luces[i][j]+cont
+        }
+    
+    }
+return cont
+
+}
+}
+
+module.exports=Tablero;
---
+++ test/letrero2Prueba.test copy.js
@@ -0,0 +1,46 @@
+const Tablero = require("../clases/tablero");
+test("crear tablero vacio", ()=>{
+    const tablero = new Tablero();
+    expect(tablero.crear()).toBe(null); 
+
+})
+test("crear tablero 4*4", ()=>{
+    const tablero = new Tablero();
+    expect(tablero.crear(4,4)).toStrictEqual([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]); 
+
+})
+test("prender luces",()=>{
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    expect(tablero.prender([0,0],[2,2])).toStrictEqual([[1, 1, 1, 0], [1, 1, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]); 
+
+})
+test("prender luces mas intenensa",()=>{
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    tablero.prender([0,0],[2,2])
+    expect(tablero.prender([0,0],[2,2])).toStrictEqual([[2, 2, 2, 0], [2, 2, 2, 0], [2, 2, 2, 0], [0, 0, 0, 0]]); 
+
+})
+test("apagar luces,",()=>{
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    tablero4x4prender=tablero.prender([0,0],[2,0]);
+    expect(tablero.apagar([0,0],[2,0])).toStrictEqual([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+})
+test("toggle",()=>{
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    tablero4x4prender=tablero.prender([0,0],[2,2]);
+    expect(tablero.cambiar([0,0],[2,3])).toStrictEqual([[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 0]]); 
+
+})
+test("cuanta hay encendidas",()=>{
+
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    tablero4x4prender=tablero.prender([0,0],[2,2]);
+    expect(tablero.cuantasEncendidas([0,0],[2,2])).toBe(9); 
+
+
+})
