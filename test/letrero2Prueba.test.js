9038d2d719a0df82e35eb961f1a596de89815264
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 10:00:38 -0300

    TDD iteration

--- test/letrero2Prueba.test.js
+++ test/letrero2Prueba.test.js
@@ -6,11 +6,6 @@ test("prender luces mas intensa",()=>{
     expect(tablero.prender([0,0],[2,2])).toStrictEqual([[2, 2, 2, 0], [2, 2, 2, 0], [2, 2, 2, 0], [0, 0, 0, 0]]); 
 
 })
-test("apagar luces menos intensa,",()=>{
-    const tablero = new Tablero();
-    tablero4x4=tablero.crear(4,4);
-    tablero4x4prender=tablero.prender([0,0],[2,0]);
-    expect(tablero.apagar([0,0],[2,0])).toStrictEqual([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-})
+
 
 
