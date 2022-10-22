3e27078d77224b68081253f57c10b76e8207df0f
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 09:28:19 -0300

    TDD iteration

--- clases/tablero.js
+++ clases/tablero.js
@@ -45,7 +45,7 @@ this.cambiar=function(posinicial,posfinal){
     for(var i=posinicial[0];i<=posfinal[0];i++){
         for (var j = posinicial[1]; j<=posfinal[1]; j++){
 
-            this.luces[i][j]=(this.luces[i][j]==0)?1:0;
+            this.luces[i][j]=(this.luces[i][j]===0)?1:0;
 
         }
     }
