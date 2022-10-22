1d9ef943e75e68b5ee43e632866173902d3819e9
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 10:16:18 -0300

    TDD iteration

--- test/letrero2Prueba.test.js
+++ test/letrero2Prueba.test.js
@@ -1,4 +1,4 @@
-const Tablero = require("../clases/tablero");
+const Tablero = require("../clases/tablero(premodificado)");
 test("prender luces mas intensa",()=>{
     const tablero = new Tablero();
     tablero4x4=tablero.crear(4,4);
@@ -7,6 +7,14 @@ test("prender luces mas intensa",()=>{
 
 })
 test("apagar luces menos intensa",()=>{
+    const tablero = new Tablero();
+    tablero4x4=tablero.crear(4,4);
+    tablero.prender([0,0],[2,2])
+    tablero.prender([0,0],[2,2])
+    expect(tablero.apagar([0,0],[2,2])).toStrictEqual([[1, 1, 1, 0], [1, 1, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]); 
+
+})
+test("cambiar toggle intensidad",()=>{
     const tablero = new Tablero();
     tablero4x4=tablero.crear(4,4);
     tablero.prender([0,0],[2,2])
