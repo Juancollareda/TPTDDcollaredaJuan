6e05e448e392b630b4547864b2eca47b07b3a0ff
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-22 09:47:44 -0300

    TDD iteration

--- clases/tablero.js
+++ clases/tablero.js
@@ -58,7 +58,7 @@ this.cuantasEncendidas=function(posinicial,posfinal){
     for(var i=posinicial[0];i<=posfinal[0];i++){
         for (var j = posinicial[1]; j<=posfinal[1]; j++){
 
-            cont=(this.luces[i][j]==0)?cont+1:cont+0;
+            cont=(this.luces[i][j]==0)?cont=cont+1:cont=cont+0;
         }
     
     }
