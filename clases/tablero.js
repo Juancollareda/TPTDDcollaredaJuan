66807d9400fa15b904646e4105650a2eb94c9d4e
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-25 22:19:06 -0300

    TDD iteration

--- clases/tablero.js
+++ clases/tablero.js
@@ -21,9 +21,7 @@ return this.luces
     }
 this.prender=function(posinicial,posfinal){
 
-    if (posfinal[1]<0|| posfinal[0] < 0||posfinal[0]>length(this.luces[0])||posfinal[1]>length(luces)){
-        throw ("valor negativo")
-    }
+    
     for(var i=posinicial[0];i<=posfinal[0];i++){
         for (var j = posinicial[1]; j<=posfinal[1]; j++){
 
