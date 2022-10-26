fda9572c702da5897a0955dc0834b9b7c11e1ca2
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-10-25 22:00:24 -0300

    TDD iteration

--- clases/Errores.js
+++ clases/Errores.js
@@ -1,15 +1,5 @@
 
 function Error(){
-    this.numeroNegativo=function(numero){
-    if (numero>0){
-        throw ("Error en las dimensiones establecidas")
-    }
-}
-    this.numeroEnRangoNumericoPositivo=function(cord1,cord2,lista){
-    
-    if (cord2[0]<length(lista) || cord2[1]<length ){
-        throw("Error en el rango establecido")
-    }
-}
+  
 }
 module.exports=Error
